71ee4e0e7129622cb43a6bbcc07b772855011a1e
Oscar Calles <ocalles@gmail.com>
2013-12-03 13:50:15 +0100

    Cocoa Programming L29  L30 - Core Data
    Cocoa Programming L29 L30 - Core Data

---
+++ CocoaExamples4/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  CocoaExamples4
+//
+//  Created by Oscar Calles on 12/3/13.
+//  Copyright (c) 2013 ocalles@gmail.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSPersistentDocument
+
+@end
